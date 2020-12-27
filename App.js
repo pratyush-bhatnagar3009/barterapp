2c9ed98068b166a3118b31a6f55e4622671cba06
pratyush-bhatnagar3009 <64519337+pratyush-bhatnagar3009@users.noreply.github.com>
2020-12-27 21:18:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './Screens/WelcomeScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <WelcomeScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
